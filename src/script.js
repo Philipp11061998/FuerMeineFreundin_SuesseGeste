f5a45d7fd2a8da058b9252d2f85287a05ab1d3c7
Philipp11061998 <philippkraut25@gmail.com>
2024-07-03 02:46:39 +0200

    Kleines Geschenk für meine Freundin und eine gute Übung für mich. Moments und Home funktionieren Lokal. In Chat arbeite ich gerade an der Serveranbindung

---
+++ src/script.js
@@ -0,0 +1,76 @@
+$(document).ready(function() {
+    $("#hamburgLabel").click(function() {
+        $(this).toggleClass('clicked');
+        $("#nav").toggle(); // Toggle visibility of the navigation menu
+    });
+
+    $("#ausklappen").click(function() {
+            $("#NavigationMoments").toggle();
+            let visNavMom = $("#NavigationMoments").css("display");
+
+            if (visNavMom !== "none") {
+                $("#ausklappen").html("Tage einklappen <span class='arrow'>&#x2191</span>");
+            } else {
+                $("#ausklappen").html("Tage ausklappen <span class='arrow'>&#x2193</span>");
+            }
+    });
+
+    $("#NavigationMoments").click(function(){
+        $("#NavigationMoments").toggle();
+            let visNavMom = $("#NavigationMoments").css("display");
+
+            if (visNavMom !== "none") {
+                $("#ausklappen").html("Tage einklappen <span class='arrow'>&#x2191</span>");
+            } else {
+                $("#ausklappen").html("Tage ausklappen <span class='arrow'>&#x2193</span>");
+            }
+    });
+
+    $("#sendMessage").click(function() {
+        const userInput = $("#message").val();
+    
+        $.ajax({
+            url: '../sensitive-data/sendmessageJasmin.php',
+            type: 'POST',
+            data: {
+                message: userInput,
+            },
+            success: function(response) {
+                console.log('Message sent successfully:', response);
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.error('AJAX error:', textStatus, errorThrown);
+                console.log('Server response:', jqXHR.responseText);
+            }
+        });
+    });
+    
+    
+
+    /*Das hier ist der Aufruf des Sendens .php
+    sendMessage() {
+        const formData = new FormData();
+        formData.append('message', this.message);
+        formData.append('csrf_token', this.csrfToken);
+
+        fetch('sendmessageJasmin.php', {
+            method: 'POST',
+            body: formData
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text(); // Hier können Sie die Serverantwort verarbeiten, wenn benötigt
+        })
+        .then(data => {
+            console.log('Message sent successfully:', data); // Optional: Serverantwort verarbeiten
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+    }
+        */
+
+});
+
